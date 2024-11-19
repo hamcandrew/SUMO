7c0ce1e07cd69187b76edadecba86bab0e61517a
hamcandr <hamcandr@cougarnet.uh.edu>
2024-11-19 14:29:51 -0600

    blastscript commit

--- identify_genes/Blast_SAE.sh
+++ identify_genes/Blast_SAE.sh
@@ -4,3 +4,5 @@
 #SBATCH --mem=60Gb
 makeblastdb -in /project/stuckert/hamcandr/sumo_project/combined_assembly/trinity_assembly.Trinity.fasta -dbtype nucl -out
 blastn -query SAE1.fasta -db SAE_db -out SAE_db.out -outfmt 6
+blastn -query UBC9.fasta -db SAE_db -out UBC9_db.out -outfmt 6
+blastn -query SAE2.fasta -db SAE_db -out SAE2_db.out -outfmt 6
