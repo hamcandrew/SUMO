735c415f1a9de3048cc2106d3d6d95e6302c14ff
hamcandr <hamcandr@cougarnet.uh.edu>
2024-11-19 20:15:00 -0600

    commit nulliscript

---
+++ gene_quant/Nulliparous_expression.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#SBATCH -t 2-00:00
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=60Gb
+#SBATCH --output=nulli_calculate_result.txt
+/project/stuckert/hamcandr/sumo_project/gene_quant/RSEM-1.3.3/rsem-calculate-expression --bowtie2 \
+ /project/stuckert/hamcandr/sumo_project/SRR12749779.fastq.gz \
+ /project/stuckert/hamcandr/sumo_project/gene_quant/reference_name \
+ nulli_expression
